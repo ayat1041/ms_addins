46c5d921d60d4704bcb5c985bb07cabf9e77c90e
ayat1041 <ayat.dihan6593@gmail.com>
2023-09-04 16:40:58 +0600

    done ghaghati table

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -52,25 +52,26 @@ async function createTable() {
 
     // TODO1: Queue table creation logic here.
     const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    const expensesTable = currentWorksheet.tables.add("B1:E1", true /*hasHeaders*/);
+    const expensesTable = currentWorksheet.tables.add("A1:E1", true /*hasHeaders*/);
     expensesTable.name = "ExpensesTable";
 
     // TODO2: Queue commands to populate the table with data.
     expensesTable.getHeaderRowRange().values =
-      [["Date", "Merchant", "Category", "Amount"]];
+      [["Date", "Merchant", "Category", "Amount","Positive"]];
 
     expensesTable.rows.add(null /*add at the end*/, [
-      ["1/1/2017", "The Phone Company", "Communications", "420"],
-      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
-      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
-      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
-      ["1/11/2017", "Bellows College", "Education", "350.1"],
-      ["1/15/2017", "Trey Research", "Other", "135"],
-      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+      ["1/1/2017", "The Phone Company", "Communications", "420","0"],
+      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33","0"],
+      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9","1"],
+      ["1/10/2017", "Coho Vineyard", "Restaurant", "33","0"],
+      ["1/11/2017", "Bellows College", "Education", "350.1","0"],
+      ["1/15/2017", "Trey Research", "Other", "135","0"],
+      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88","0"]
     ]);
 
     // TODO3: Queue commands to format the table.
     expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+    expensesTable.columns.getItemAt(4).getRange().numberFormat = [['General']];
     expensesTable.getRange().format.autofitColumns();
     expensesTable.getRange().format.autofitRows();
 
